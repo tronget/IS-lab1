eacb3d8c42a20bf65a72d071e6676ab2fda70bee
tronget <andrejlashkul@gmail.com>
2025-10-16 15:21:32 +0300

    feat: center application

--- frontend/is-lab1-client/src/App.jsx
+++ frontend/is-lab1-client/src/App.jsx
@@ -1,12 +1,26 @@
 import { Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Box,
+} from "@mui/material";
 import LabList from "./components/LabList";
 import LabDetails from "./components/LabDetails";
 import SpecialOps from "./components/SpecialOps";
 
 export default function App() {
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        width: "100vw",
+      }}
+    >
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -20,13 +34,25 @@ export default function App() {
           </Button>
         </Toolbar>
       </AppBar>
-      <Container sx={{ mt: 4 }}>
-        <Routes>
-          <Route path="/" element={<LabList />} />
-          <Route path="/lab/:id" element={<LabDetails />} />
-          <Route path="/special" element={<SpecialOps />} />
-        </Routes>
-      </Container>
-    </div>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          py: 4,
+          px: 2,
+        }}
+      >
+        <Container maxWidth="lg" sx={{ width: "100%" }}>
+          <Routes>
+            <Route path="/" element={<LabList />} />
+            <Route path="/lab/:id" element={<LabDetails />} />
+            <Route path="/special" element={<SpecialOps />} />
+          </Routes>
+        </Container>
+      </Box>
+    </Box>
   );
 }
