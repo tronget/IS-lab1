12dc7272c06fd3572a51562000bf59c69e8e0789
tronget <andrejlashkul@gmail.com>
2025-10-25 16:12:45 +0300

    fix(server): changed allowedOrigins for websocket connection endpoint

--- backend/src/main/java/com/tronget/islab1/config/WebSocketConfig.java
+++ backend/src/main/java/com/tronget/islab1/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/is-lab1")
-                .setAllowedOrigins("https://se.ifmo.ru/~s408992/**")
+                .setAllowedOrigins("https://se.ifmo.ru")
                 .withSockJS();
     }
 }
