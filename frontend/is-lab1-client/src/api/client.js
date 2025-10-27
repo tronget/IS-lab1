8f4dc916060e6309edd44fac748e753b4c8dc728
tronget <andrejlashkul@gmail.com>
2025-10-27 12:18:47 +0300

    chore: changed server port to 9091

--- frontend/is-lab1-client/src/api/client.js
+++ frontend/is-lab1-client/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://localhost:9090";
+export const API_BASE = "http://localhost:9091";
 
 export const api = axios.create({
   baseURL: API_BASE,
