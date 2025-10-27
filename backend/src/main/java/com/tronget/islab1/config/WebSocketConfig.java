01b8c9ee9027db690d98f168f4be6d12c6cfc176
tronget <andrejlashkul@gmail.com>
2025-10-27 12:08:01 +0300

    fix(server): websocket connection works

--- backend/src/main/java/com/tronget/islab1/config/WebSocketConfig.java
+++ backend/src/main/java/com/tronget/islab1/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/is-lab1")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
